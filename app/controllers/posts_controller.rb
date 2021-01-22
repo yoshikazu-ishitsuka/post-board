72656439d6c1fb4546fd817a223907b17a03faca
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-01-22 14:32:44 +0900

    投稿機能の実装#1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,26 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # byebug
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text).merge(user_id: current_user.id)
   end
 end
--- db/migrate/20210118033907_create_posts.rb
+++ db/migrate/20210118033907_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.text :text
       t.timestamps
+      t.references :user, type: :bigint, foreign_key: true
     end
   end
 end

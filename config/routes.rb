9d969a504c0d05f1cfa54c83df9805602f8f52e4
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-02-02 11:51:12 +0900

    routesの設定#6

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:create]
+  end
   root to: 'posts#index'
 end

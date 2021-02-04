2aa1a0424ddc8ad1b12c43426f3ed06661d2233a
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-02-04 15:19:14 +0900

    コメント投稿機能の実装#6

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
+    # @comment = Comment.create(comment_params)
     @comment = current_user.comments.new(comment_params)
     if @comment.save
       redirect_back(fallback_location: root_path)
@@ -11,6 +12,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require[:comment].permit(:text).merge(post_id: params[:post_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end

1fcf56fe398b1903c03d016c2df43d061514aabf
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-02-05 21:53:18 +0900

    コメント編集機能の実装#7

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def create
-    # @comment = Comment.create(comment_params)
-    @comment = current_user.comments.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
     if @comment.save
+      flash[:alert] = "コメントを投稿しました"
+      # redirect_to post_path(@post.id)
       redirect_back(fallback_location: root_path)
     else
       flash[:alert] = "コメントを（140文字以内で）入力してください。"
@@ -11,8 +14,50 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comments = @post.comments.includes(:user).order(id: "DESC")
+    render "posts/show"
+  end
+
+  def update
+    if @comment.update(comment_update_params)
+      flash[:alert] = "編集が完了しました"  
+      redirect_to post_path(@post.id)
+      ### success:green, alert:orange, notice:blue, info:light-blue, danger:red, warning:light-yellow
+    else
+      flash.now[:alert] = 'コメントを(140文字以内で)入力してください。'
+      @comments = @post.comments.includes(:user).order(id: "DESC")
+      render "posts/show"
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    flash[:alert] = "削除が完了しました"
+    redirect_to root_path
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
+
+  # def set_comment
+  #   @post = Post.find(params[:post_id])
+  #   @comment = Comment.find(params[:id])
+  # end
+
+  def comment_update_params
+    params.require(:comment).permit(:text)
+  end
+
+  def ensure_correct_user
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    # @comment = current_user.comment.find(params[:id])
+    if @comment.user_id != current_user.id
+      flash[:alert] = "権限がありません！"
+      redirect_back(fallback_location: root_path)
+    end
   end
 end

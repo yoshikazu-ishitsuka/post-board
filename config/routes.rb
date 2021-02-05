187ecba512a6aa315a0512aea76482dd2b14b038
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-02-05 17:29:47 +0900

    routesの編集#7

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :edit, :update, :destroy]
   end
   root to: 'posts#index'
 end

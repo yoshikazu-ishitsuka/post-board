af4aaf9f436f22ffd8111c0fbddc1af1a422118c
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-02-08 14:36:47 +0900

    アソシエーションの編集#8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :text, presence: true, length: { maximum: 140 }
 end

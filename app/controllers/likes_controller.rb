9be3ccf509b23059a5926f1f22d3a4b0d5ebebf9
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-02-09 10:01:15 +0900

    ログインユーザーのみいいねが出来る#8

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,8 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    @like = current_user.likes.create(post_id: params[:post_id])
+    current_user.likes.create(post_id: params[:post_id])
     redirect_back(fallback_location: root_path)
   end
 

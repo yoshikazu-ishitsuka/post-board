da9cf7629d961d96f353281077a51743c936510f
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-02-10 18:07:41 +0900

    userモデルのバリデーションの設定#9

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,21 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  VALID_EMAIL_REGEX = /\A[[a-z0-9_]+\-.]+@[a-z\d\-]+\.[a-z]+\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+
+  validates :name, presence: true, length: { maximum: 20 }
+
+  validates :email,
+    presence: true,
+    format: { with: VALID_EMAIL_REGEX, allow_blank: true },
+    uniqueness: true
+  
+  validates :password,
+    presence: true,
+    format: { with: VALID_PASSWORD_REGEX, message: :invalid_password, allow_blank: true }
+
+
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end

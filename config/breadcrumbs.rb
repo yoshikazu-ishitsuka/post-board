2916048d0005eb276d7e07fbfdbc1039663a961d
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-02-13 11:29:37 +0900

    breadcrumbs.rbの編集#19

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,5 +1,20 @@
 crumb :root do
-  link "Home", root_path
+  link "投稿一覧", root_path
+end
+
+crumb :show_post do |post|
+  link "#{post.user.name}さんの投稿(#{post.id})", post_path
+  parent :root
+end
+
+crumb :new_post do
+  link "新規投稿", new_post_path
+  parent :root
+end
+
+crumb :edit_post do |post|
+  link "#{post.user.name}さんの投稿(#{post.id})の編集", edit_post_path
+  parent :root
 end
 
 # crumb :projects do
